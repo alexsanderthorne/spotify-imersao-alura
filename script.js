e143c3d9625835df7cef8221834c52146e90d34f
alexsanderthorne <alexsanderthorne@gmail.com>
2025-02-04 12:56:57 -0300

    implementing function displayResult with result.filter

--- script.js
+++ script.js
@@ -3,18 +3,32 @@ const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result))
+        .then((result) => displayResults(result, searchTerm))
 }
 
-function displayResults(result) {
-    resultPlaylist.classList.add("hidden")
+function displayResults(result, searchTerm) {
+    resultPlaylist.classList.add("hidden");
     const artistName = document.getElementById('artist-name');
     const artistImage = document.getElementById('artist-img');
 
-    result.forEach(element => {
+    if (result.length === 0) {
+        artistName.innerText = 'No results found';
+        artistImage.src = '';
+        return;
+    }
+
+    const filteredResults = result.filter(element => element.name.toLowerCase().includes(searchTerm));
+
+    if (filteredResults.length === 0) {
+        artistName.innerText = 'No results found';
+        artistImage.src = '';
+        return;
+    }
+
+    filteredResults.forEach(element => {
         artistName.innerText = element.name;
         artistImage.src = element.urlImg;
     });
@@ -27,7 +41,7 @@ document.addEventListener('input', function () {
     if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
         resultArtist.classList.remove('hidden');
-        return
+        return;
     }
     
     requestApi(searchTerm);
